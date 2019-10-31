c22048278895a57039c80fa6282d96501f177f91
Hagiu Stefan Bogdan <stefanhagiu@gmial.com>
2019-10-31 20:45:34 +0200

    serve static files

--- http-server/server.js
+++ http-server/server.js
@@ -4,11 +4,11 @@ const port = 9000;
 
 app.get('/', (_, res) => res.send('Hello World!'));
 
-
 app.post('/', function (_, res) { res.send('Got a POST request') });
 
 app.put('/', function (_, res) { res.send('Got a PUT request') });
 
 app.delete('/', function (_, res) { res.send('Got a DELETE request') });
 
+app.use(express.static('public'));
 app.listen(port, () => console.log(`Server start at port ${port}`));
