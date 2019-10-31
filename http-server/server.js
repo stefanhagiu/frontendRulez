72989ad07bed2c8daba49d87e14c9b532470feb3
Hagiu Stefan Bogdan <stefanhagiu@gmial.com>
2019-10-31 20:32:45 +0200

    simple put

--- http-server/server.js
+++ http-server/server.js
@@ -7,4 +7,6 @@ app.get('/', (_, res) => res.send('Hello World!'));
 
 app.post('/', function (_, res) { res.send('Got a POST request') });
 
+app.put('/', function (_, res) { res.send('Got a PUT request') });
+
 app.listen(port, () => console.log(`Server start at port ${port}`));
