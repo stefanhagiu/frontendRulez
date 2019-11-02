40620df5d4386a41e9a2aef0c5a54b95b7f7b621
Hagiu Stefan Bogdan <stefanhagiu@gmial.com>
2019-11-02 11:54:39 +0200

    error codes update

--- http-server/server.js
+++ http-server/server.js
@@ -3,7 +3,30 @@ const app = express();
 const port = 9000;
 
 app.get('/', (_, res) => res.send('Hello World!'));
-app.get('/500', (a, b) => { throw Error('Server Error') });
+app.put('/201', (_, res) => res.status(201).send('Created'));
+app.get('/202', (_, res) => res.status(202).send('Accepted'));
+app.get('/203', (_, res) => res.status(203).send('Non-Authoritativ Information'));
+app.get('/204', (_, res) => res.status(204).send('No Content'));
+
+
+app.get('/301', (_, res) => res.status(301).send('Moved Permanently'));
+app.get('/302', (_, res) => res.status(302).send('Found'));
+app.get('/303', (_, res) => res.status(303).send('See Other'));
+app.get('/304', (_, res) => res.status(304).send('Not Modified'));
+
+
+app.get('/400', (_, res) => res.status(400).send('Bad request'));
+app.get('/401', (_, res) => res.status(401).send('Unauthorized'));
+app.get('/402', (_, res) => res.status(402).send('Payment Required'));
+app.get('/403', (_, res) => res.status(403).send('Forbidden'));
+app.get('/405', (_, res) => res.status(405).send('Method not Allowed'));
+app.delete('/405', (_, res) => res.send('Method is Allowed'));
+
+app.get('/500', (_, res) => { res.status(500).send('Internet server error') });
+app.get('/501', (_, res) => { res.status(501).send('Not Implemented') });
+app.delete('/501', (_, res) => { res.send('Is Implemented') });
+app.get('/502', (_, res) => { res.status(502).send('Bad Gateway') });
+app.get('/503', (_, res) => { res.status(503).send('Service Unavailable') });
 
 app.post('/', function (_, res) { res.send('Got a POST request') });
 
@@ -11,13 +34,14 @@ app.put('/', function (_, res) { res.send('Got a PUT request') });
 
 app.delete('/', function (_, res) { res.send('Got a DELETE request') });
 
-
+/*
 app.use(function(_, res, _) { res.status(404).send("Could not find the URI requested")});
 
 app.use(function(err, _, res, _) {
     console.error(err.stack);
     res.status(500).send('Something broke!');
 });
+*/
 
 app.use(express.static('public'));
 app.listen(port, () => console.log(`Server start at port ${port}`));
